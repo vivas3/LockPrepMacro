b3ae18212c9b77042fc8b39deaed0df0c1bb26f0
Visa <visa.saarinen@tuni.fi>
2021-11-29 12:02:31 +0200

    3v3 and 5v5 working

--- LockPrepMacro.lua
+++ LockPrepMacro.lua
@@ -83,74 +83,56 @@ function UpdateLockPrep()
             [12] = "/run for i=0,4 do for x=1,GetContainerNumSlots(i) do y=GetContainerItemLink(i,x) if y then if string.find(GetItemInfo(y),'Healthstone') ~= nil then UseContainerItem(i,x) return end end end end\n/run UpdateLockPrep()",
             [13] = "/run AcceptTrade()\n/run UpdateLockPrep()",
         }
-        
         if not UnitExists('pet') then
-            -- Summon Imp
+            -- Summon Imp if no pet exists
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif (NoBuffExists('player', 'Fire Shield') or NoBuffExists('party1', 'Fire Shield')) and UnitCreatureFamily('pet') ~= "Imp" then
-            -- Summon Imp
+            -- Summon Imp if missing Fire Shield and pet is not Imp
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif NoBuffExists('party1', 'Fire Shield') or NoBuffExists('party1', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on party1
             LockPrepMacro:SetAttribute("macrotext", texts[2])
-
         elseif NoBuffExists('player', 'Fire Shield') or NoBuffExists('player', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on player
             LockPrepMacro:SetAttribute("macrotext", texts[3])
-
         elseif NoBuffExists('party1', 'Detect Invisibility') then
             -- Cast Detect Invisibility on party1
             LockPrepMacro:SetAttribute("macrotext", texts[4])
-
         elseif NoBuffExists('player', 'Detect Invisibility') then
             -- Cast Detect Invisibility on player
             LockPrepMacro:SetAttribute("macrotext", texts[5])
-
         elseif NoBuffExists('player', 'Fel Armor') then
             -- Cast Fel Armor
             LockPrepMacro:SetAttribute("macrotext", texts[6])
-
         elseif UnitCreatureFamily('pet') == "Imp" and NoCastingSpecificSpell('Summon Voidwalker') then
             -- Summon Voidwalker
             LockPrepMacro:SetAttribute("macrotext", texts[7])
-
         elseif not IsEquippedItem('Master Spellstone') and NoItemExists('Master Spellstone') then
             -- Cast Master Spellstone
             LockPrepMacro:SetAttribute("macrotext", texts[8])
-
         elseif not NoItemExists('Master Spellstone') and not IsEquippedItem('Master Spellstone') then
             -- Equip Master Spellstone
             LockPrepMacro:SetAttribute("macrotext", texts[9])
-
         elseif not HSTraded and NoItemExists('Healthstone') then
             -- Cast HS
             LockPrepMacro:SetAttribute("macrotext", texts[10])
-
         elseif not HSTraded and not NoItemExists('Healthstone') and not TradeFrame:IsShown() then
             -- Initiate Trade
             LockPrepMacro:SetAttribute("macrotext", texts[11])
-
         elseif not HSTraded and TradeFrame:IsShown() and NoTradingHS() then
             -- Trade HS
             LockPrepMacro:SetAttribute("macrotext", texts[12])
-
         elseif TradeFrame:IsShown() and not HSTraded then
             -- Accept Trade
             LockPrepMacro:SetAttribute("macrotext", texts[13])
-
         elseif HSTraded and NoItemExists('Healthstone') then
             -- Cast HS
             LockPrepMacro:SetAttribute("macrotext", texts[10])
-
         else
             -- Do nothing
             LockPrepMacro:SetAttribute("macrotext", "/stopcasting\n/run UpdateLockPrep()")
-
         end
     elseif ArenaCheck(3) then
-        -- @TODO 3v3 sequences. Should be same as except for the Ritual of Souls instead of HS.
         local texts = {
             [1] = "/cast [nopet] Summon Imp\n/stopcasting [pet:Imp]\n/run UpdateLockPrep()",
             [2] = "/cast [@party1] Fire Shield\n/cast [@party1] Unending Breath\n/run UpdateLockPrep()",
@@ -166,68 +148,55 @@ function UpdateLockPrep()
             [12] = "/cast Ritual of Souls\n/run UpdateLockPrep()",
         }
         if not UnitExists('pet') then
-            -- Summon Imp
+            -- Summon Imp if no pet exists
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif (NoBuffExists('player', 'Fire Shield') or NoBuffExists('party1', 'Fire Shield') or NoBuffExists('party2', 'Fire Shield')) and UnitCreatureFamily('pet') ~= "Imp" then
-            -- Summon Imp
+            -- Summon Imp if missing Fire Shield and pet is not Imp
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif NoBuffExists('party1', 'Fire Shield') or NoBuffExists('party1', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on party1
             LockPrepMacro:SetAttribute("macrotext", texts[2])
         elseif NoBuffExists('party2', 'Fire Shield') or NoBuffExists('party2', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on party2
             LockPrepMacro:SetAttribute("macrotext", texts[3])
-
         elseif NoBuffExists('player', 'Fire Shield') or NoBuffExists('player', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on player
             LockPrepMacro:SetAttribute("macrotext", texts[4])
-
         elseif NoBuffExists('party1', 'Detect Invisibility') then
             -- Cast Detect Invisibility on party1
             LockPrepMacro:SetAttribute("macrotext", texts[5])
         elseif NoBuffExists('party2', 'Detect Invisibility') then
             -- Cast Detect Invisibility on party2
             LockPrepMacro:SetAttribute("macrotext", texts[6])
-
         elseif NoBuffExists('player', 'Detect Invisibility') then
             -- Cast Detect Invisibility on player
             LockPrepMacro:SetAttribute("macrotext", texts[7])
-
         elseif NoBuffExists('player', 'Fel Armor') then
             -- Cast Fel Armor
             LockPrepMacro:SetAttribute("macrotext", texts[8])
-
         elseif UnitCreatureFamily('pet') == "Imp" and NoCastingSpecificSpell('Summon Voidwalker') then
             -- Summon Voidwalker
             LockPrepMacro:SetAttribute("macrotext", texts[9])
-
         elseif not IsEquippedItem('Master Spellstone') and NoItemExists('Master Spellstone') then
-            -- Cast Master Spellstone
+            -- Cast Master Spellstone if it's not equipped and not in bags
             LockPrepMacro:SetAttribute("macrotext", texts[10])
-
         elseif not NoItemExists('Master Spellstone') and not IsEquippedItem('Master Spellstone') then
-            -- Equip Master Spellstone
+            -- Equip Master Spellstone if it exists
             LockPrepMacro:SetAttribute("macrotext", texts[11])
-
         elseif RoSReady() and NoCastingSpecificSpell('Ritual of Souls') then
             -- HS for all!!!
             LockPrepMacro:SetAttribute("macrotext", texts[12])
-
         else
             -- Do nothing
             LockPrepMacro:SetAttribute("macrotext", "/stopcasting\n/run UpdateLockPrep()")
-
         end
     elseif ArenaCheck(5) then
-        -- @TODO 5v5 sequences
         local texts = {
             [1] = "/cast [nopet] Summon Imp\n/stopcasting [pet:Imp]\n/run UpdateLockPrep()",
             [2] = "/cast [@party1] Fire Shield\n/cast [@party1] Unending Breath\n/run UpdateLockPrep()",
             [3] = "/cast [@party2] Fire Shield\n/cast [@party2] Unending Breath\n/run UpdateLockPrep()",
-            [4] = "/cast [@party3] Fire Shield\n/cast [@party1] Unending Breath\n/run UpdateLockPrep()",
-            [5] = "/cast [@party4] Fire Shield\n/cast [@party2] Unending Breath\n/run UpdateLockPrep()",
+            [4] = "/cast [@party3] Fire Shield\n/cast [@party3] Unending Breath\n/run UpdateLockPrep()",
+            [5] = "/cast [@party4] Fire Shield\n/cast [@party4] Unending Breath\n/run UpdateLockPrep()",
             [6] = "/cast [@player] Fire Shield\n/cast [@player] Unending Breath\n/run UpdateLockPrep()",
             [7] = "/cast [@party1] Detect Invisibility\n/run UpdateLockPrep()",
             [8] = "/cast [@party2] Detect Invisibility\n/run UpdateLockPrep()",
@@ -241,13 +210,11 @@ function UpdateLockPrep()
             [16] = "/cast Ritual of Souls\n/run UpdateLockPrep()",
         }
         if not UnitExists('pet') then
-            -- Summon Imp
+            -- Summon Imp if no pet exists
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif (NoBuffExists('player', 'Fire Shield') or NoBuffExists('party1', 'Fire Shield') or NoBuffExists('party2', 'Fire Shield') or NoBuffExists('party3', 'Fire Shield') or NoBuffExists('party4', 'Fire Shield')) and UnitCreatureFamily('pet') ~= "Imp" then
-            -- Summon Imp
+            -- Summon Imp if missing Fire Shield and pet is not Imp
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif NoBuffExists('party1', 'Fire Shield') or NoBuffExists('party1', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on party1
             LockPrepMacro:SetAttribute("macrotext", texts[2])
@@ -260,11 +227,9 @@ function UpdateLockPrep()
         elseif NoBuffExists('party4', 'Fire Shield') or NoBuffExists('party4', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on party4
             LockPrepMacro:SetAttribute("macrotext", texts[5])
-
         elseif NoBuffExists('player', 'Fire Shield') or NoBuffExists('player', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on player
             LockPrepMacro:SetAttribute("macrotext", texts[6])
-
         elseif NoBuffExists('party1', 'Detect Invisibility') then
             -- Cast Detect Invisibility on party1
             LockPrepMacro:SetAttribute("macrotext", texts[7])
@@ -277,35 +242,27 @@ function UpdateLockPrep()
         elseif NoBuffExists('party4', 'Detect Invisibility') then
             -- Cast Detect Invisibility on party4
             LockPrepMacro:SetAttribute("macrotext", texts[10])
-
         elseif NoBuffExists('player', 'Detect Invisibility') then
             -- Cast Detect Invisibility on player
             LockPrepMacro:SetAttribute("macrotext", texts[11])
-
         elseif NoBuffExists('player', 'Fel Armor') then
             -- Cast Fel Armor
             LockPrepMacro:SetAttribute("macrotext", texts[12])
-
         elseif UnitCreatureFamily('pet') == "Imp" and NoCastingSpecificSpell('Summon Voidwalker') then
             -- Summon Voidwalker
             LockPrepMacro:SetAttribute("macrotext", texts[13])
-
         elseif not IsEquippedItem('Master Spellstone') and NoItemExists('Master Spellstone') then
-            -- Cast Master Spellstone
+            -- Cast Master Spellstone if it's not equipped and not in bags
             LockPrepMacro:SetAttribute("macrotext", texts[14])
-
         elseif not NoItemExists('Master Spellstone') and not IsEquippedItem('Master Spellstone') then
-            -- Equip Master Spellstone
+            -- Equip Master Spellstone if it exists
             LockPrepMacro:SetAttribute("macrotext", texts[15])
-
         elseif RoSReady() and NoCastingSpecificSpell('Ritual of Souls') then
             -- HS for all!!!
             LockPrepMacro:SetAttribute("macrotext", texts[16])
-
         else
             -- Do nothing
             LockPrepMacro:SetAttribute("macrotext", "/stopcasting\n/run UpdateLockPrep()")
-
         end
     else
         local texts = {
@@ -319,45 +276,35 @@ function UpdateLockPrep()
             [10] = "/cast Create Healthstone\n/run UpdateLockPrep()",
         }
         if not UnitExists('pet') then
-            -- Summon Imp
+            -- Summon Imp if no pet exists
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif NoBuffExists('player', 'Fire Shield') and UnitCreatureFamily('pet') ~= "Imp" then
-            -- Summon Imp
+            -- Summon Imp if missing Fire Shield and pet is not Imp
             LockPrepMacro:SetAttribute("macrotext", texts[1])
-
         elseif NoBuffExists('player', 'Fire Shield') or NoBuffExists('player', 'Unending Breath') then
             -- Cast Fire Shield and Unending Breath on player
             LockPrepMacro:SetAttribute("macrotext", texts[3])
-
         elseif NoBuffExists('player', 'Detect Invisibility') then
             -- Cast Detect Invisibility on player
             LockPrepMacro:SetAttribute("macrotext", texts[5])
-
         elseif NoBuffExists('player', 'Fel Armor') then
             -- Cast Fel Armor
             LockPrepMacro:SetAttribute("macrotext", texts[6])
-
         elseif UnitCreatureFamily('pet') == "Imp" then
             -- Summon Voidwalker
             LockPrepMacro:SetAttribute("macrotext", texts[7])
-
         elseif not IsEquippedItem('Master Spellstone') and NoItemExists('Master Spellstone') then
-            -- Cast Master Spellstone
+            -- Cast Master Spellstone if it's not equipped and not in bags
             LockPrepMacro:SetAttribute("macrotext", texts[8])
-
         elseif not NoItemExists('Master Spellstone') and not IsEquippedItem('Master Spellstone') then
-            -- Equip Master Spellstone
+            -- Equip Master Spellstone if it exists
             LockPrepMacro:SetAttribute("macrotext", texts[9])
-
         elseif NoItemExists('Healthstone') then
             -- Cast HS
             LockPrepMacro:SetAttribute("macrotext", texts[10])
-
         else
             -- Do nothing
             LockPrepMacro:SetAttribute("macrotext", "/stopcasting\n/run UpdateLockPrep()")
-
         end
     end
 end
